d4a3b3260c7399f81ad685673a096a5f61520f2c
rongshuai zhan <1014216913@qq.com>
2025-10-02 20:06:17 +0800

    fix daily task

--- src/tasks/DailyTask.py
+++ src/tasks/DailyTask.py
@@ -26,7 +26,7 @@ class DailyTask(MyBaseTask):
 
     def run(self):
         self.log_info('日常任务开始运行!', notify=True)
-        self.into_game()
+        self.clickOcr(match='进入游戏', time=4)
         self.close_login_tip_jiacheng()
         self.daily_shop()
         self.daily_HuaHeZhan_Task()
@@ -34,202 +34,72 @@ class DailyTask(MyBaseTask):
         self.log_info('日常任务运行完成!', notify=True)
 
     def daily_shop(self):
-        if (self.into_Shopping()):
-            self.sleep(1)
-            if (self.into_Gift()):
-                self.sleep(1)
-                if (self.into_DailyGift()):
-                    self.sleep(1)
-                    if (self.ocrClick(0.15, 0.40, match="免费")):
-                        self.pressKeyRandom('esc')
-                    self.back_DailyGift()
-                    self.sleep(1)
-                    self.back_Gift()
-                    self.sleep(1)
-                else:
-                    self.back_DailyGift()
-                    self.sleep(1)
+        if (self.clickImg(match='home_shop')):
+            if (self.clickOcr(0.87,0.94,1,1, match="礼包屋")):
+                if (self.clickOcr(0.92,0.51,1,1, match="日常")):
+                    if self.clickOcr(0.15, 0.40, match="免费"): self.exitPage()
+                    #退出礼包屋
+                    self.exitPage()
+                    #退出商店
+                    self.exitPage()
             else:
-                self.back_Shopping()
-                self.sleep(1)
+                #退出商店
+                self.exitPage()
 
     def daily_HuaHeZhan_Task(self):
-        if (self.into_HuaHeZhan()):
-            self.sleep(1)
-            self.clickRandom(0.95,0.45)
-            self.sleep(.5)
-            if (self.into_HuaHeZhan_pick_getAll()):
-                self.sleep(2)
-                self.clickRandom(0.59,0.91)
-                self.sleep(.5)
-                self.clickRandom(0.03,0.05)
-                self.sleep(.5)
-            else:
-                self.clickRandom(0.03,0.05)
-                self.sleep(.5)
-        else:
-            self.clickRandom(0.03,0.05)
-            self.sleep(.5)
+        if (self.clickImg(0.5,0.7,match='home_huahezhan',time=2)):
+            self.clickImg(0.93,0.45,match="home_huahezhan_task")
+            if self.clickOcr(0.68,0.78,1,1, match="领取", time=2): self.clickRandom(0.59,0.91)
+            #退出花合战
+            self.clickRandom(0.01,0.03)
 
     def daily_YinYangLiao(self):
-        if (self.ocrFind(match="阴阳寮")):
-            self.clickRandom(0.44,0.88)
-            self.sleep(2)
+        if (self.findOcr(match="阴阳寮")):
+            self.clickRandom(0.44,0.88,2)
             # 打开结界
-            if (self.ocrClick(0.80, 0.90, match="结界")):
-                self.sleep(2)
+            if (self.clickImg(match="yinyangliao_jiejie", time=2)):
                 # 打开结界卡
                 self.clickRandom(0.74,0.47)
-                self.sleep(1)
-                # if (self.check_ActiveJiejie()):
-                # self.sleep(1)
+                if (self.findOcr(0.7, 0.8, match='卸下')):
+                    # 关掉寄养界面
+                    self.exitPage()
+                    # 退出结界
+                    self.exitPage()
+                    # 退出阴阳寮
+                    self.clickRandom(0.04, 0.06, 2)
+                    return None
                 # 打开全部
                 self.clickRandom(0.34,0.17)
-                self.sleep(1)
                 # 选择太鼓
                 self.clickRandom(0.34,0.38)
-                self.sleep(1)
                 # 选第一张太鼓
                 self.clickRandom(0.25,0.31)
-                self.sleep(1)
                 # 点击激活
                 self.clickRandom(0.85,0.83)
-                self.sleep(1)
-                # 点击确定 是否将寄养位置
+                # 点击确定 是否将寄养位置 设为私立/公开
                 self.clickRandom(0.60, 0.60)
-                self.sleep(1)
                 # 关掉寄养界面
-                self.clickRandom(0.92, 0.16)
-                self.sleep(1)
+                self.exitPage()
                 # 退出结界
-                self.clickRandom(0.04, 0.06)
-                self.sleep(2)
+                self.exitPage()
                 # 退出阴阳寮
-                self.clickRandom(0.04, 0.06)
-                self.sleep(2)
+                self.clickRandom(0.04, 0.06, 2)
+                return None
             else: 
                 # 退出阴阳寮
-                self.clickRandom(0.04, 0.06)
-                self.sleep(2)
+                self.clickRandom(0.04, 0.06, 2)
                 return None
-
-    def into_game(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="进入游戏")
-        if (box):
-            self.clickRandomBox(box)
-            self.sleep(4)
-            return box
-
-    def into_Shopping(self):
-        box = self.ocrClick(0,0,1,1, match="商店")
-        if (box):
-            return True
-        # self.clickRandom(0.54,0.89)
-        # return True
-        return None
     
     def back_Shopping(self):
         self.clickRandom(0.03,0.03)
 
-    def into_Gift(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="礼包屋")
-        if (box):
-            self.clickRandomBox(box)
-            return box
-
     def back_Gift(self):
         self.clickRandom(46,55)
 
-    def into_DailyGift(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="日常")
-        if (box):
-            self.clickRandomBox(box)
-            return box
-
     def back_DailyGift(self):
         self.clickRandom(37,22)
-
-    def into_HuaHeZhan(self):
-        # box = self.find_box_by_ocr(0,0,1,1, match="花合战")
-        # if (box):
-        #     self.clickRandomBox(box)
-        self.clickRandom(0.62,0.89)
-        return True
-
-    def into_HuaHeZhan_pick_getAll(self):
-        box = self.find_box_by_ocr(0.68,0.78,1,1, match="领取")
-        if (box):
-            self.clickRandomBox(box)
-            return box
-        # self.click(0.62,0.89)
-
-    def into_Zhaohuan(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="召唤")
-        if (box):
-            self.clickRandomBox(box)
-            return box
-    
-    def into_Explor(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="探索")
-        if (box):
-            self.clickRandomBox(box)
-            return box
-            
-    def into_Jiejie(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="结界突破")
-        if (box):
-            self.clickRandomBox(box)
-            return box
-            
-    def into_Jiejie_Person(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="个人")
-        if (box):
-            self.clickRandomBox(box)
-            return box
-            
-    def into_YinYangLiao(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="阴阳寮")
-        if (box):
-            self.clickRandomBox(box)
-            return box
-            
-    def check_ActiveJiejie(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="激活")
-        if (box):
-            self.clickRandomBox(box)
-            return box
             
     def close_login_tip_jiacheng(self):
-        box = self.find_box_by_ocr(0,0,1,1, match="是否打开大人之前被自动关")
-        if (box):
+        if (self.findOcr(match="是否打开大人之前被自动关")):
             self.clickRandom(0.41,0.58)
-            self.sleep(1)
-            return box
-
-    def find_some_text_on_bottom_right(self):
-        return self.ocr(box="bottom_right",match="进入游戏", log=True) #指定box以提高ocr速度
-
-    def find_some_text_with_relative_box(self):
-        return self.ocr(0.5, 0.5, 1, 1, match=re.compile("招"), log=True) #指定box以提高ocr速度
-
-    def test_find_one_feature(self):
-        return self.find_one('box_battle_1')
-
-    def test_find_feature_list(self):
-        return self.find_feature('box_battle_1')
-
-    def run_for_5(self):
-        self.operate(lambda: self.do_run_for_5())
-
-    def do_run_for_5(self):
-        self.do_send_key_down('w')
-        self.sleep(0.1)
-        self.do_mouse_down(key='right')
-        self.sleep(0.1)
-        self.do_mouse_up(key='right')
-        self.sleep(5)
-        self.do_send_key_up('w')
-
-
-
-
+            return True
--- src/tasks/MyBaseTask.py
+++ src/tasks/MyBaseTask.py
@@ -9,6 +9,7 @@ class MyBaseTask(BaseTask):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    # 公共方法
     def operate(self, func):
         self.executor.interaction.operate(func, block=True)
 
@@ -51,17 +52,27 @@ class MyBaseTask(BaseTask):
         else:
             logger.info(f"❌ 未找到匹配 '{match}' 的Box")
             return None
-
-    def clickRandom(self, x, y):
-        down_time = 0.2 + random.uniform(-0.01, 0.01)
-        self.click(x, y, down_time)
-
-    def clickRandomBox(self, box):
-        down_time = 0.2 + random.uniform(-0.01, 0.01)
-        self.click(box, down_time)
+        
+    def find_box_by_cv(self, x, y, to_x, to_y, match):
+        box_list = self.find_feature(match)
+        if box_list:
+            first_box = box_list[0]
+            # 计算相对坐标百分比
+            x_percent = first_box.x / 1280
+            y_percent = first_box.y / 720
+            logger.info(f"✅ 找到目标Box: '{first_box.name}' (置信度: {first_box.confidence:.2f})")
+            logger.info(f"  相对百分比: ({x_percent:.3f}, {y_percent:.3f})")
+            return first_box
+        else:
+            logger.info(f"❌ 未找到匹配 '{match}' 的Box")
+            return None
+        
+    def getRound(self, base=0, min=0.1, max=0.6, fixed=1):
+        result = base + random.uniform(min, max)
+        return round(result, fixed)
 
     def pressKeyRandom(self, key):
-        down_time = 0.2 + random.uniform(-0.01, 0.01)
+        down_time = self.getRound(0.2)
         self.send_key(key, down_time)
 
     def click_box_area_random(self, box, margin=5):
@@ -89,12 +100,14 @@ class MyBaseTask(BaseTask):
             target_x = random.randint(int(min_x), int(max_x))
             target_y = random.randint(int(min_y), int(max_y))
         
-        logger.info(f"🎯 区域随机点击: ({target_x}, {target_y})")
         
-        self.clickRandom(target_x, target_y)
+        down_time = self.getRound(0.2)
+
+        logger.info(f"🎯 区域随机点击: ({target_x}, {target_y}, {down_time})")
+        self.click(target_x, target_y, down_time)
         return True
 
-    def click_box_relative_random(self, box, screen_width=1280, screen_height=720):
+    def click_box_relative_random(self, box, x, y, screen_width=1280, screen_height=720):
         """使用相对坐标的随机点击（适合不同分辨率）
         
         Args:
@@ -102,17 +115,19 @@ class MyBaseTask(BaseTask):
             screen_width: 屏幕宽度
             screen_height: 屏幕高度
         """
-        if not box:
+        if box:
+            x_center, y_center = box.center()
+            x_percent = x_center / screen_width
+            y_percent = y_center / screen_height
+        elif x > 0:
+            x_percent = x
+            y_percent = y
+        else:
             return False
         
-        # 计算相对坐标
-        x_center, y_center = box.center()
-        x_percent = x_center / screen_width
-        y_percent = y_center / screen_height
-        
         # 在相对坐标基础上添加随机偏移
-        x_offset_percent = random.uniform(-0.01, 0.01)  # ±1%的随机偏移
-        y_offset_percent = random.uniform(-0.01, 0.01)
+        x_offset_percent = self.getRound(0,0.005,0.01, 3)  # ±1%的随机偏移
+        y_offset_percent = self.getRound(0,0.005,0.01, 3)
         
         final_x_percent = x_percent + x_offset_percent
         final_y_percent = y_percent + y_offset_percent
@@ -124,19 +139,55 @@ class MyBaseTask(BaseTask):
         logger.info(f"   基础相对坐标: ({x_percent:.4f}, {y_percent:.4f})")
         logger.info(f"   最终相对坐标: ({final_x_percent:.4f}, {final_y_percent:.4f})")
         
-        self.click(final_x_percent, final_y_percent)
+        down_time = self.getRound()
+        self.click(final_x_percent, final_y_percent, down_time)
         return True
     
-    def ocrFind(self, x1 = 0, y1 = 0, tox = 1, toy = 1, match = ""):
+    def clickRandom(self, x, y, time=1):
+        self.click_box_relative_random(None,x,y)
+        if (time > 0):
+            self._sleep(time)
+
+    def clickRandomBox(self, box, time=1):
+        self.click_box_area_random(box)
+        if (time > 0):
+            self._sleep(time)
+
+    def closeErrorDialog(self):
+        # if (self.clickImg('')):
+            # self._sleep(1)
+            logger.info(f"closeErrorDialog ")
+
+    def exitPage(self):
+        self.pressKeyRandom('esc')
+        self._sleep(1)
+    
+    # 外层api
+
+    def findOcr(self, x1 = 0, y1 = 0, tox = 1, toy = 1, match = ""):
         box = self.find_box_by_ocr(x1, y1, tox, toy, match)
         return box
 
-    def ocrClick(self, x1 = 0, y1 = 0, tox = 1, toy = 1, match = ""):
+    def clickOcr(self, x1 = 0, y1 = 0, tox = 1, toy = 1, match = "", time = 1):
         box = self.find_box_by_ocr(x1, y1, tox, toy, match)
         if (box):
-            self.click_box_area_random(box)
+            self.clickRandomBox(box, time)
             return box
         else:
             return None
 
+    def clickImg(self, x1 = 0, y1 = 0, tox = 1, toy = 1, match = "", time = 1):
+        box = self.find_box_by_cv(x1, y1, tox, toy, match)
+        if (box):
+            self.clickRandomBox(box, time)
+            return box
+        else:
+            return None
 
+    def _sleep(self, time = 1):
+        timemin = min(1, time + 0.5)
+        timemax = max(1, time + 1)
+        timerandom = random.randint(timemin, timemax)
+        logger.info(f" wait '{timerandom}' second")
+        self.sleep(timerandom)
+        self.closeErrorDialog()
